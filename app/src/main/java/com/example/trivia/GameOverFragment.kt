c8c5b458093b481ca7cc679b54488a06e0f337e5
Paresh Dudhat <dudhatparesh@gmail.com>
2020-12-22 11:44:11 +0530

    Navigation controller

---
+++ app/src/main/java/com/example/trivia/GameOverFragment.kt
@@ -0,0 +1,41 @@
+package com.example.trivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.trivia.databinding.FragmentGameOverBinding
+import com.example.trivia.databinding.FragmentGameWonBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameOverFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameOverFragment : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding : FragmentGameOverBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_over, container, false)
+        binding.tryAgainButton.setOnClickListener{ view: View -> view.findNavController().navigate(R.id.action_gameOverFragment2_to_gameFragment)
+
+        }
+
+        return  binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/trivia/GameWonFragment.kt
@@ -0,0 +1,37 @@
+package com.example.trivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.trivia.databinding.FragmentGameWonBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameWonFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameWonFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding : FragmentGameWonBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchButton.setOnClickListener{view : View -> view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)}
+         return  binding.root
+    }
+
+
+}
